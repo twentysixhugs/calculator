14689dab70e56584939e3cb05eb5e6deba0cf86a
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-20 11:50:30 +0300

    Build composite command increasing current value

---
+++ src/modules/commands/IncreaseCurrentValueCommand.ts
@@ -0,0 +1,20 @@
+import SetCurrentValueCommand from "./SetCurrentValueCommand";
+import { ICommand } from "../interfaces/Command.interface";
+import { ICalculator } from "../interfaces/Calculator.interface";
+
+export default class IncreaseCurrentValueCommand implements ICommand {
+  constructor(private receiver: ICalculator, private arg: number) {
+    this.receiver = receiver;
+    this.arg = arg;
+  }
+
+  execute() {
+    const currentValue = this.receiver.getCurrentValue().toString();
+    const valueToAdd = this.arg.toString();
+
+    new SetCurrentValueCommand(
+      this.receiver,
+      Number(currentValue + valueToAdd)
+    ).execute();
+  }
+}
