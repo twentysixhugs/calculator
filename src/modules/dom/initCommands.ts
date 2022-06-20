6c2563a5438b5a97a73e339f8e6a84a2a11b9c40
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-20 13:15:41 +0300

    Pass display as command receiver, add validation

--- src/modules/dom/initCommands.ts
+++ src/modules/dom/initCommands.ts
@@ -1,23 +1,33 @@
 import Calculator from "../Calculator";
 import AppendCurrentValueCommand from "../commands/AppendCurrentValueCommand";
+import Display from "../Display";
 import { ICalculator } from "../interfaces/Calculator.interface";
+import { IDisplay } from "../interfaces/Display.interface";
 
 export default function initCommands() {
   const calculator = new Calculator();
+  const display = new Display();
 
-  initCurrentValueUpdate(calculator);
+  try {
+    initCurrentValueUpdate(calculator, display);
+  } catch (err) {
+    // TODO: Show on screen
+    console.log(err);
+  }
 }
 
-function initCurrentValueUpdate(receiver: ICalculator) {
-  // increase current value when clicking on 0-9
+function initCurrentValueUpdate(calculator: ICalculator, display: IDisplay) {
+  // append current value when clicking on 0-9
   // 1, 2, 2, 4 results in 1224
-
   document.querySelectorAll<HTMLButtonElement>(".js-number").forEach((el) => {
     el.addEventListener("click", (e) => {
-      new AppendCurrentValueCommand(
-        receiver,
-        Number((e.target as HTMLButtonElement).id)
-      ).execute();
+      const number = Number((e.target as HTMLButtonElement).id);
+
+      if (number >= 0 && number <= 9) {
+        new AppendCurrentValueCommand(calculator, display, number).execute();
+      } else {
+        throw new Error("Incorrect input");
+      }
     });
   });
 }
