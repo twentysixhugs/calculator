ba9313ddc53a101655bfa68b89e49be2fe832fc8
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-21 12:17:45 +0300

    Change export default to export

--- src/modules/commands/ReciprocalCommand.ts
+++ src/modules/commands/ReciprocalCommand.ts
@@ -1,7 +1,7 @@
 import { ICommand } from "../interfaces/Command.interface";
 import { ICalculator } from "../interfaces/Calculator.interface";
 
-export default class ReciprocalCommand implements ICommand {
+export class ReciprocalCommand implements ICommand {
   constructor(private calculator: ICalculator) {
     this.calculator = calculator;
   }
