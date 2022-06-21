d7804af8b96f341baabd773fd27c09a053db3158
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-21 14:02:27 +0300

    Add commands for manipulating operators and operations

---
+++ src/modules/commands/SetOperatorCommand.ts
@@ -0,0 +1,14 @@
+import { ICommand } from "../interfaces/Command.interface";
+import { ICalculator } from "../interfaces/Calculator.interface";
+import { Operator } from "../constants";
+
+export class SetOperatorCommand implements ICommand {
+  constructor(
+    private calculator: ICalculator,
+    private operator: Operator | null
+  ) {}
+
+  execute() {
+    this.calculator.setOperator(this.operator);
+  }
+}
