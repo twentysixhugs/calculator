bea7adf62472d364986f093205f58ef26feb8823
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-22 14:52:55 +0300

    Change command name

--- src/modules/commands/InvertSignCommand.ts
+++ src/modules/commands/InvertSignCommand.ts
@@ -1,20 +1,18 @@
 import { ICommand } from "../interfaces/Command.interface";
 import { ICalculator } from "../interfaces/Calculator.interface";
 
-export class ChangeOperandSignCommand implements ICommand {
+export class InvertSignCommand implements ICommand {
   constructor(
     private calculator: ICalculator,
     private operandPosition: "left" | "right"
   ) {}
 
   execute() {
-    const operandValue = this.calculator.getOperand(this.operandPosition);
+    const operand = this.calculator.getOperand(this.operandPosition);
 
-    if (!operandValue) {
-      return false;
-    }
+    if (!operand) return false;
 
-    this.calculator.setOperand(this.operandPosition, -operandValue);
+    this.calculator.setOperand(this.operandPosition, -operand);
     return true;
   }
 }
