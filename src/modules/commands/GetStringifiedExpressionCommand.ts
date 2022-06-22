b94a5f564fa23f0be14fbaf1a45c984e82a254c9
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-22 12:07:38 +0300

    Fix grammar

--- src/modules/commands/GetStringifiedExpressionCommand.ts
+++ src/modules/commands/GetStringifiedExpressionCommand.ts
@@ -6,7 +6,7 @@ export class GetStringifiedExpressionCommand implements ICommand {
   constructor(private calculator: ICalculator) {}
 
   execute() {
-    // Returns stringified version of current expression
+    // Returns a stringified version of the current expression
     const { left, operator, right } = this.calculator.getExpression();
 
     return `${left ?? ""}${operator ? OperatorCharacters[operator] : ""}${
