0037e90d2e39911ebc7656aa9be339b2c8121bb7
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-22 15:00:01 +0300

    Add commands for x^2, x^3 and 10^x

---
+++ src/modules/commands/PowerOfThreeCommand.ts
@@ -0,0 +1,19 @@
+import { ICommand } from "../interfaces/Command.interface";
+import { ICalculator } from "../interfaces/Calculator.interface";
+
+export class PowerOfThreeCommand implements ICommand {
+  constructor(
+    private calculator: ICalculator,
+    private operandPosition: "left" | "right"
+  ) {}
+
+  execute() {
+    const operand = this.calculator.getOperand(this.operandPosition);
+    if (!operand) return false;
+
+    const result = this.calculator.power(operand, 3);
+    this.calculator.setOperand(this.operandPosition, result);
+
+    return true;
+  }
+}
