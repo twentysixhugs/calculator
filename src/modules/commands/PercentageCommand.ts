956b280e8f41d123d86ae31ca4c3f42d1dbadea9
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-22 14:54:03 +0300

    Query operand using position

--- src/modules/commands/PercentageCommand.ts
+++ src/modules/commands/PercentageCommand.ts
@@ -2,9 +2,18 @@ import { ICommand } from "../interfaces/Command.interface";
 import { ICalculator } from "../interfaces/Calculator.interface";
 
 export class PercentageCommand implements ICommand {
-  constructor(private calculator: ICalculator, private operand: number) {}
+  constructor(
+    private calculator: ICalculator,
+    private operandPosition: "left" | "right"
+  ) {}
 
   execute() {
-    return this.calculator.percentage(this.operand);
+    const operand = this.calculator.getOperand(this.operandPosition);
+    if (!operand) return false;
+
+    const result = this.calculator.percentage(operand);
+    this.calculator.setOperand(this.operandPosition, result);
+
+    return true;
   }
 }
--- src/modules/commands/ReciprocalCommand.ts
+++ src/modules/commands/ReciprocalCommand.ts
@@ -2,9 +2,18 @@ import { ICommand } from "../interfaces/Command.interface";
 import { ICalculator } from "../interfaces/Calculator.interface";
 
 export class ReciprocalCommand implements ICommand {
-  constructor(private calculator: ICalculator, private operand: number) {}
+  constructor(
+    private calculator: ICalculator,
+    private operandPosition: "left" | "right"
+  ) {}
 
   execute() {
-    return this.calculator.reciprocal(this.operand);
+    const operand = this.calculator.getOperand(this.operandPosition);
+    if (!operand) return false;
+
+    const result = this.calculator.reciprocal(operand);
+    this.calculator.setOperand(this.operandPosition, result);
+
+    return true;
   }
 }
