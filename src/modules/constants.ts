baebe4d6a3217898c846bebeb48d35fa2f6c1ffb
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-22 14:53:44 +0300

    Add enum for immediate operations selectors

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -11,3 +11,16 @@ export enum OperatorCharacters {
   Multiply = "\u00d7",
   Divide = "\u002F",
 }
+
+export enum ImmediateOperations {
+  // Each member has a corresponding HTML data-operation attribute
+  TenPowerX = "[data-operation='10-power-x']",
+  SquareRoot = "[data-operation='square-root']",
+  CubicRoot = "[data-operation='cubicroot']",
+  OneDividedByX = "[data-operation='1-divided-by-x']",
+  Factorial = "[data-operation='factorial']",
+  InvertSign = "[data-operation='invert-sign']",
+  XPowerTwo = "[data-operation='x-power-2']",
+  XPowerThree = "[data-operation='x-power-3']",
+  Percentage = "[data-operation='percentage']",
+}
