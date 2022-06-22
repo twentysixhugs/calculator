f46f44a38347ae0f9414847fb74ff80c3ca49b9d
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-22 14:52:49 +0300

    Add commands for cubic and square roots and factorial

---
+++ src/modules/commands/FactorialCommand.ts
@@ -0,0 +1,19 @@
+import { ICommand } from "../interfaces/Command.interface";
+import { ICalculator } from "../interfaces/Calculator.interface";
+
+export class FactorialCommand implements ICommand {
+  constructor(
+    private calculator: ICalculator,
+    private operandPosition: "left" | "right"
+  ) {}
+
+  execute() {
+    const operand = this.calculator.getOperand(this.operandPosition);
+    if (!operand) return false;
+
+    const result = this.calculator.factorial(operand);
+    this.calculator.setOperand(this.operandPosition, result);
+
+    return true;
+  }
+}
