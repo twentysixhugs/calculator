955ff34a818761c02efd62464b846c9af850d089
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-26 10:11:00 +0300

    Fix: decimal point state is not checked on memory recall

--- src/modules/commands/Memory/RecallFromMemoryCommand.ts
+++ src/modules/commands/Memory/RecallFromMemoryCommand.ts
@@ -5,8 +5,17 @@ export class RecallFromMemoryCommand implements ICommand {
   constructor(private calculator: ICalculator) {}
 
   execute() {
-    this.calculator.setOperand("left", this.calculator.recallFromMemory());
+    const valueFromMemory = this.calculator.recallFromMemory();
+    this.calculator.setOperand("left", valueFromMemory);
 
+    if (valueFromMemory.toString().includes(".")) {
+      this.calculator.setDecimalPointState(true, "left");
+    } else {
+      this.calculator.setDecimalPointState(false, "left");
+    }
+
+    // Reset decimal point and expression state
+    this.calculator.setDecimalPointState(false, "right");
     this.calculator.setOperator(null);
     this.calculator.setOperand("right", null);
   }
